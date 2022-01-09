cb52eae56fd2d5a8e66fc022c65b697aa1a49237
BooMajka <maya@Marias-iMac.local>
2022-01-09 12:34:51 -0600

    game logic

--- __tests__/random.test.js
+++ __tests__/random.test.js
@@ -4,9 +4,9 @@
 //   expect(checkIfEqual(10, 10)).toBe(true);
 // });
 
-// const randomNumber = require("../lib/random.js");
+const randomNumber = require("../lib/random.js");
 
-// test("gets random number between 1 and 10", () => {
-//   expect(randomNumber()).toBeGreaterThanOrEqual(1);
-//   expect(randomNumber()).toBeLessThanOrEqual(10);
-// });
+test("gets random number between 1 and 10", () => {
+  expect(randomNumber()).toBeGreaterThanOrEqual(1);
+  expect(randomNumber()).toBeLessThanOrEqual(10);
+});
